363451b95038db9c712f071dcf0de5415b1b1afc
CodeKhushal <khushalsachdeva22@gmail.com>
2023-01-07 22:07:43 +0530

    map-react-app deployment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { MapContainer as LeafletMap, CircleMarker, TileLayer, Popup } from "react-leaflet";
+import data from './components/cities'
+import "leaflet/dist/leaflet.css";
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    function getColor(data) {
+      if (data <= 300) return '#1E90FF'
+      else if (data > 300 && data <= 500) return '#4169E1'
+      else if (data > 500 && data <= 1000) return '#0000FF'
+      else return '#00008B'
+    }
+    return (
+      <div className='container'>
+        <h2 style={{ width: "fit-content", margin: "auto", marginTop: "10px", marginBottom: "10px" }}>Data Usage as per the region</h2>
+        <LeafletMap
+          className='static-map'
+          style={{ height: "600px", width: "90%", margin: "auto", padding: "10px" }}
+          zoom={1.5}
+          zoomControl={false}
+          // scrollWheelZoom={false}
+          center={[5, 51.505]}>
+          <TileLayer url="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+          {data.city.map((city, area) => {
+            return (
+              <CircleMarker
+                center={[city["coordinates"][1], city["coordinates"][0]]}
+                radius={20 * Math.log(city["data"] / 100)}
+                fillColor={getColor(city["data"])}
+                fillOpacity={0.5}
+                stroke={false}
+              >
+                <Popup>
+                  <span>{city["name"] + ", DATA: " + city["data"]}</span>
+                </Popup>
+              </CircleMarker>
+            )
+          })
+          }
+        </LeafletMap>
+
+        <div className="referenceData" style={{ marginTop: "20px", marginLeft: "4%", marginRight: "4%", width: "100%", boxSizing: "border-box" }}>
+          <span>$ Usage</span>
+          <svg width='100%' height='40px' padding='5px' margin='auto'>
+            <g>
+              <rect fill='#1E90FF' width='92%' height='25'></rect>
+              <rect fill='#4169E1' width='75%' height='25'></rect>
+              <rect fill='#0000FF' width='57%' height='25'></rect>
+              <rect fill='#00008B' width='37%' height='25'></rect>
+            </g>
+          </svg>
+          <svg width='100%' height='50px' margin='auto' marginTop='-5px'>
+            <g>
+              <rect fill='#00008B' width='15px' height='15'></rect>
+              <text fill='black' x='25' y='14'>{">$5K"}</text>
+              <text fill='black' x='15%' y='15'>36%</text>
+
+              <rect fill='#0000FF' x='37%' y='1' width='15px' height='15'></rect>
+              <text fill='black' x='39%' y='15'>{"$1K-5K"}</text>
+              <text fill='black' x='47%' y='15'>24%</text>
+
+              <rect fill='#4169E1' x='57%' y='1' width='15px' height='15'></rect>
+              <text fill='black' x='59%' y='15'>{"$500-1K"}</text>
+              <text fill='black' x='67%' y='15'>20%</text>
+
+              <rect fill='#1E90FF' x='75%' y='1' width='15px' height='15'></rect>
+              <text fill='black' x='77%' y='15'>{"<$500"}</text>
+              <text fill='black' x='86%' y='15'>20%</text>
+            </g>
+          </svg>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/cities.js
@@ -0,0 +1,16 @@
+let data = {
+    city: [
+        {"id":"1", "name":"North America", "coordinates": [-100.0902, 40.7129], "region": "US", "data": 690},
+        {"id":"2", "name":"South America", "coordinates": [-60.0902, -10.7129], "region": "US", "data": 1490},
+        {"id":"3", "name":"Shanghai", "coordinates": [121.4737, 31.2304], "region": "EA", "data": 790},
+        {"id":"4", "name":"Mumbai", "coordinates": [72.8777, 19.0760], "region": "SE", "data": 490},
+        {"id":"5", "name":"Istanbul", "coordinates": [28.9784, 41.0082], "region": "MEA", "data": 190},
+        {"id":"6", "name":"Tokyo", "coordinates": [139.6917, 35.6895], "region": "EA", "data": 390},
+    ],
+    minLat: -6.1751,
+    maxLat: 55.7558,
+    minLong: 37.6173,
+    maxLong: 139.6917
+}
+
+export default data
